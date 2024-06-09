a4d7dff1e927eddf3d41db3274657be6f8e979d0
Daniil <folco1999@gmail.com>
2024-06-09 19:34:41 +0200

    connect image upload with user, by providing user id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import "~/styles/globals.css";
 import "@uploadthing/react/styles.css";
-
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
 import { ClerkProvider } from "@clerk/nextjs";
+ 
+import { ourFileRouter } from "~/app/api/uploadthing/core";
 import { TopNav } from "./_components/topnav";
 
 import { GeistSans } from "geist/font/sans";
@@ -20,6 +23,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
+        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <body className="flex flex-col gap-4">
           <TopNav />
           {children}
