659926732c22be4f460daa18cecfbdc6d1977414
Daniil <folco1999@gmail.com>
2024-06-09 15:10:32 +0200

    set up authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,26 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
-
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
-    <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48 flex flex-col gap-2">
-            <img src={image.url} alt="image" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
-    </main>
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col gap-2">
+          <img src={image.url} alt="image" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
   );
+}
+
+export default async function HomePage() {
+  return (
+  <main className="">
+    <Images />
+  </main>);
 }
