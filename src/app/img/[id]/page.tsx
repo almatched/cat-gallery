e3fc32349f9ca746fd9a96b78bad0260a9281453
Daniil <folco1999@gmail.com>
2024-06-10 13:24:01 +0200

    make modal for image and a separate page for image when modal refreshed

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,5 +1,4 @@
 import ImagePageView from "~/components/image-page-view";
-import { Modal } from "./modal";
 
 export default function ImagePage({
   params: { id: photoId },
@@ -10,9 +9,5 @@ export default function ImagePage({
 
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
 
-  return (
-    <Modal>
-      <ImagePageView id={idAsNumber} />
-    </Modal>
-  );
+  return <ImagePageView id={idAsNumber} />;
 }
