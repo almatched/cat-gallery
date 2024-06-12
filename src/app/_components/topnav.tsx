735db70cb945beac10f3d84ae057e9566bbd9e9f
Daniil <folco1999@gmail.com>
2024-06-12 20:33:05 +0200

    make gallery logo in topnav a link to go to home page

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,10 +1,12 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
 import { UploadButton } from "./upload-button";
 
 export function TopNav() {
   return (
     <nav className="font-semiboldg flex w-full items-center justify-between border-b p-4 text-xl">
-      <div>Gallery</div>
+      <Link href="/" className="cursor-pointer">Gallery</Link>
       <div className="flex gap-4 items-center">
         <UploadButton />
         <SignedOut>
